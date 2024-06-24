232cad1b869bef9682e2215c0bd6dca8ef430ca2
Faq89 <facundoledesma89@gmail.com>
2024-06-24 19:26:04 -0300

    yani

---
+++ src/Registrarse.jsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource react */
+import HeaderPrivado from "./components/HeaderPrivado";   
+import Footer from "./components/Footer";
+import Background from "./components/Background";
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import './components/Footer.css';
+import './components/Header.css';
+import './components/Background.css';
+import './components/General.css'
+import RegisterUser from "./components/RegisterUser";
+import App from "./App";
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<Background/>
+<App/>
+<RegisterUser/>
+<Footer/>
+</React.StrictMode>
+)
